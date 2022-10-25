ce11c0d79e448f3f0364e12ee1206e7e6dcb4683
DongyoPark-VIRNECT <dypark@virnect.com>
2022-10-25 15:39:41 +0900

    chat

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,19 +1,38 @@
+const messageList = document.querySelector('ul');
+const messageForm = document.querySelector('#message');
+const nickNameForm = document.querySelector('#nickname');
+
 const socket = new WebSocket(`ws://${window.location.host}`);
 
+function makeMessage(type, payload) {
+  const message = { type, payload };
+  return JSON.stringify(message);
+}
+
 socket.addEventListener("open", () => {
-    console.log("Connected to the Server ✅");
-    socket.send("hi i am browser");
+  console.log("Connected to the Server ✅");
 });
 
 socket.addEventListener("message", (message) => {
-  console.log(message);
-    console.log("New message: ", message.data);
+  const li = document.createElement("li");
+  li.innerText = message.data;
+  messageList.append(li);
 });
 
 socket.addEventListener("close", () => {
-    console.log("Disconnected from the Server ❌");
+  console.log("Disconnected from the Server ❌");
 });
 
-setTimeout(() => {
-  socket.send("hello from the browser!");
-},1000);
+messageForm.addEventListener("submit", (event)=>{
+  event.preventDefault();
+  const input = messageForm.querySelector("input");
+  socket.send(makeMessage("new_message", input.value));
+  input.value="";
+});
+
+nickNameForm.addEventListener("submit", (event)=>{
+  event.preventDefault();
+  const input = nickNameForm.querySelector("input");
+  socket.send(makeMessage("nickname", input.value));
+  input.value="";
+});
--- src/server.js
+++ src/server.js
@@ -14,12 +14,28 @@ const handleListen = () => console.log(`Listening on http://localhost:3000`);
 
 const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
+let sockets = [];
 
 wss.on("connection", (socket)=>{
-    socket.send("hello from the server");
-    socket.on("message", (message)=>{
-        console.log(message);
-    });
-    socket.on("close", ()=> console.log("Disconnected from the Browser"));
+	sockets.push(socket);
+	socket["nickname"] = "Anonnomous";
+	socket.send("hello from the server 👋");
+	socket.on("message", (message)=>{
+		const parsed = JSON.parse(message);
+		switch(parsed.type)
+		{
+			case "new_message":
+				sockets.forEach(aSocket => aSocket.send(`${socket.nickname} : ${parsed.payload}`));
+				break;
+			case "nickname":
+				socket["nickname"] = parsed.payload;
+				break;
+
+		}
+	});
+	socket.on("close", ()=> {
+		console.log("Disconnected from the Browser")
+		sockets = sockets.filter(aSocket => aSocket !== socket);
+	});
 });
 server.listen(3000, handleListen);
